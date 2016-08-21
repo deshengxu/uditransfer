13a3fc5b4aea569af9443cc089d642329471cec5
xudesheng <xudesheng@gmail.com>
2016-08-20 22:15:14 -0400

    added date timestamp to log file name.

--- uditransfer/util.py
+++ uditransfer/util.py
@@ -1,6 +1,7 @@
 import logging
 import os.path
 import sys
+import datetime
 
 def initialize_logger(output_dir, stream_loglevel = logging.INFO, all_loglevel=logging.DEBUG):
     try:
@@ -19,15 +20,19 @@ def initialize_logger(output_dir, stream_loglevel = logging.INFO, all_loglevel=l
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
+    now = datetime.datetime.now()
+    error_log_filename = "error-%4d-%02d-%02d.log" % (now.year, now.month, now.day)
+    all_log_filename = "all-%4d-%02d-%02d.log" % (now.year, now.month, now.day)
+
     # create error file handler and set level to error
-    handler = logging.FileHandler(os.path.join(output_dir, "error.log"), "a+", encoding=None, delay="true")
+    handler = logging.FileHandler(os.path.join(output_dir, error_log_filename), "a+", encoding=None, delay="true")
     handler.setLevel(logging.ERROR)
     formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     # create debug file handler and set level to debug
-    handler = logging.FileHandler(os.path.join(output_dir, "all.log"), "a+")
+    handler = logging.FileHandler(os.path.join(output_dir, all_log_filename), "a+")
     handler.setLevel(all_loglevel)
     formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     handler.setFormatter(formatter)
